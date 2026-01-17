81f8b748d27d74cb44770ee638560ac572a77f7d
Caskiuz <rijarwow@gmail.com>
2026-01-17 00:10:04 -0400

    Add missing auth middleware for Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
+            'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
             'rol.contable' => \App\Http\Middleware\RolContable::class,
         ]);
         // Desactivar CSRF temporalmente para solucionar error 419
